95b902c53fd8d3dabddb1523be0701c0c994fb07
Eli Cusic <tntitans31@gmail.com>
2021-04-03 08:36:30 -0500

    altered controller for better test output

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,6 +1,6 @@
 class ThingsController < ApplicationController
     def index 
-        @things = Thing.all{ |thing| thing.words }
+        @things = Thing.all
         render json: @things
     end
 end
