4232414bdb5e43fa7ee821cc3bbd4b35d72a767a
Eli Cusic <tntitans31@gmail.com>
2021-04-03 09:47:30 -0500

    set up create method to see if we can create through heroku

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -3,4 +3,11 @@ class ThingsController < ApplicationController
         @things = Thing.all
         render json: @things
     end
+
+    def create
+        # binding.pry
+        new_words = params["_json"]
+        @thing = Thing.create(words: new_words)
+        render json: @thing
+    end
 end
